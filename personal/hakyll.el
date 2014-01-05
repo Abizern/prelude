16746afe5e01eb6b6e601b495b536757af4c3aff
Abizer Nasir <abizern@abizern.org>
2014-01-05 03:43:55 +0000

    Add a macro to create a new hakyll post
    Hardcoded location for the site, though. But YAGNI.

---
+++ personal/hakyll.el
@@ -0,0 +1,29 @@
+;;; hakyll.el --- Convenience functions for working with Hakyll.
+;;
+;;
+;;; Commentary:
+;; Convenience functions for working with Hakyll.
+
+;;; Code:
+
+;; Hardcode location of Hakyll site.
+(defvar hakyll-site-location)
+(setq hakyll-site-location "~/Sites/hblog/")
+
+(defun hakyll-new-post (title tags)
+  "Create a new Hakyll post for today with TITLE and TAGS."
+  (interactive "sTitle: \nsTags: ")
+  (let ((file-name (hakyll-title title)))
+    (set-buffer (get-buffer-create file-name))
+    (progn (markdown-mode))
+    (insert (format "---\ntitle: %s\ntags: %s\ndescription: \n---\n\n" title tags))
+    (write-file (expand-file-name file-name (concat hakyll-site-location "posts")))
+    (switch-to-buffer file-name)
+    ))
+
+(defun hakyll-title (title)
+  "Return a name based on TITLE to use for the file name."
+  (concat (format-time-string "%Y-%m-%d") "-" (replace-regexp-in-string " " "-" title) ".md")
+  )
+
+;;; hakyll.el ends here
