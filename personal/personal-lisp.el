0d626c5e1de36f23cf366bbdd37aca89c3274297
Abizer Nasir <abizern@abizern.org>
2014-03-31 15:29:56 +0100

    Create personal-lisp.el
    Initially, this just sets clisp as common lisp

---
+++ personal/personal-lisp.el
@@ -0,0 +1,13 @@
+;;; personal-lisp.el --- Personal Lisp Settings
+;;
+;;; Commentary:
+;;  Personal settings for editing Common Lisp files
+
+;;; Code:
+
+;; Set up clisp as the default lisp instead of ccl
+(setq slime-default-lisp 'clisp)
+
+
+(provide 'personal-lisp)
+;;; personal-lisp.el ends here
